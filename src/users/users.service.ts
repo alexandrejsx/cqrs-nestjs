3e2ac25a938411d4b354e22d69e71dca485503b1
Alexandre Queiroz <alexandrequeiroz.dev@gmail.com>
2024-03-15 22:18:27 -0300

    feature: user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from '../models/user.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
+  async createUser(name: string, email: string): Promise<User> {
+    const createdUser = new this.userModel({ name, email });
+    return await createdUser.save();
+  }
+
+  async getUsers(): Promise<User[]> {
+    return await this.userModel.find().exec();
+  }
+}
