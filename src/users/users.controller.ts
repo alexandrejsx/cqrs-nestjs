852c8a61651c5210287e19d85ff1aa4ec12e4b1b
Alexandre Queiroz <alexandrequeiroz.dev@gmail.com>
2024-03-21 21:04:05 -0300

    feature: user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from '../models/user.model';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(
+    @Body('name') name: string,
+    @Body('email') email: string,
+  ): Promise<User> {
+    return await this.usersService.createUser(name, email);
+  }
+
+  @Get()
+  async getUsers(): Promise<User[]> {
+    return await this.usersService.getUsers();
+  }
+}
